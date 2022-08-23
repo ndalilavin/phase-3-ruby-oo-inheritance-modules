95426efcb9c4f3940b188eb29cce5294e7703b31
Ndalila <vinson.steve@student.moringaschool.com>
2022-08-24 00:32:53 +0300

    Done.

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,17 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+
+class Dancer 
+    # extend FancyDance::ClassMethods
+    # include FancyDance::InstanceMethods
+
+    include Dance 
+    #include FancyDance
+
+    extend MetaDancing
+
+    attr_accessor :name 
+    def initialize(name)
+        @name = name
+    end
+end
